eef393de32cdf225395b2d8ea131748b6ada521a
Chris Winstead <chris.winstead@usu.edu>
2023-10-06 15:06:37 -0400

    Added SystemC code used for NGDBF simulations.

---
+++ SystemC/NGDBF/inc/ldpcsim.h
@@ -0,0 +1,121 @@
+/*=========================================================================
+** ldpcsim.h
+** Date: Sept, 2011
+** By Chris Winstead
+      Department of Electrical and Computer Engineering
+      Utah State University
+      chris.winstead@usu.edu
+** Description: 
+   Top header file for ldpcsim tool.
+==========================================================================*/
+
+#ifndef LDPCSIM_H
+#define LDPCSIM_H
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+#include "alist.h"
+
+// ===================================================================
+// Type for internal messages between check nodes and symbol nodes:
+// ===================================================================
+typedef double message_type;
+
+
+//================================
+// Struct simparams for storing
+// global parameters:
+//================================
+typedef struct 
+{
+  // Stopping and reporting parameters:
+  int iterations_per_frame;  // Maximum iterations per frame
+  int total_clock_cycles;    // Maximum iterations between incremental progress reports
+
+  // Structural parameters:
+  alist_struct  alist;  // Alist specification for the code's parity-check matrix
+  int           N;      // Number of symbol nodes
+  int           M;      // Number of check nodes
+  int           dc;     // Maximum dc
+  int           dv;     // Maximum dv
+  double        Rate;   // Code rate
+
+  // Testbench I/O files:
+  char *        stimfilename;
+  string        fname;
+
+  // Adaptation parameters:
+  double  theta;
+  double  lambda;
+
+  // Channel parameters:
+  double        SNR;          // Signal to Noise Ratio (Eb/N0) in dB
+  double        sigma;        // Channel noise standard deviation
+  double        N0;
+
+  // Quantization parameters:
+  unsigned int  precision;    // Number of bits for message magnitudes
+  unsigned int  Nq;           // Number of distinct levels in quantization
+  double        Ymax;         // Maximum magnitude for quantization
+  unsigned int  nEdges;       // Total number of edges in the code graph
+  double        alpha;        // Syndrome weight parameter
+} simparams;
+
+// Declare a global parameter struct available to all modules:
+extern simparams p;
+extern vector<double> qthresholds;
+extern vector<double> qvalues;
+
+//=============================
+// Function Predefines:
+//=============================
+void get_arguments(int argc, char * argv[]);
+void append_result_to_data_file(unsigned int errors, unsigned int totalbits, unsigned int word_errors, unsigned int total_words, unsigned long totalIterations);
+
+
+//---------------------------------------------------
+// quantize function
+// simulates uniform quantization in the range
+// from -Ymax to +Ymax
+//---------------------------------------------------
+void initializeQuantization()
+{
+  qthresholds.resize(p.Nq-1);
+  qvalues.resize(p.Nq);
+  for (int i=0; i<p.Nq-1; i++)
+    {
+      qthresholds[i] = -p.Ymax*(p.Nq-2.0)/(p.Nq-1.0) + i*(2.0*p.Ymax/(p.Nq-1.0));
+      qvalues[i] = -p.Ymax + i*(2.0*p.Ymax/(p.Nq-1.0));
+    }
+  qvalues[p.Nq-1] = p.Ymax;
+}
+
+double quantize(double Y)
+{ 
+  /*
+  double rval = Y;
+  if (rval > p.Ymax)
+    rval = p.Ymax;
+  else if (rval < -p.Ymax)
+    rval = -p.Ymax;
+  */
+  //double Qmax = pow(2.0,p.precision); 
+
+  int k = 0;
+  for (int i=0; i<p.Nq-1; i++)
+    {
+      if (Y > qthresholds[i])
+	  k = i+1;
+    }
+  //  double Qval = round(rval*Qmax/p.Ymax)*p.Ymax/Qmax;
+  //  return Qval;
+  return qvalues[k];
+}
+
+
+
+#endif
---
+++ SystemC/NGDBF/src/ldpcsim.cpp
@@ -0,0 +1,139 @@
+/*==========================================================================================
+** ldpcsim.cpp
+** Date: Sept, 2011
+** By Chris Winstead
+      Department of Electrical and Computer Engineering
+      Utah State University
+      chris.winstead@usu.edu
+** Description: 
+
+==============================================================================================*/
+
+
+#include "alist.h"
+#include "ldpcsim.h"
+#include "LDPC_testbench.h"
+#include "nodes.h"
+#include "decoder.h"
+
+#include <iostream>
+#include <fstream>
+
+// Global simulation parameters:
+simparams p;
+vector<double> qthresholds;
+vector<double> qvalues;
+
+//=====================================
+// SC_MAIN BODY
+//=====================================
+int sc_main(int argc, char * argv[])
+{
+  get_arguments(argc, argv);
+
+  // -----------------------------------------
+  // Declare Top-Level Signals:
+  // ----------------------------------------- 
+  sc_vector<sc_signal<message_type > >	 y("y",p.N);
+  sc_vector<sc_signal<bool>  >	     	 d("d",p.N);      
+  sc_signal<bool>	     		 rst;
+  sc_signal<bool>                        ready;
+  sc_signal<bool>                        finished;
+  sc_clock				 clk("clock", 10, SC_NS);
+
+  // Testbench modules:
+  LDPC_testbench	tb("tb");
+  decoder              dec("dec");
+  
+
+  // -----------------------------------------
+  // Connect global clock and reset signals,
+  // and the main input/output signals:
+  // -----------------------------------------
+  
+  for (int i=0; i<p.N; i++)
+    {
+      tb.y[i](y[i]);
+      dec.y[i](y[i]);
+      tb.d[i](d[i]);
+      dec.d[i](d[i]);
+    }
+
+  tb.clk(clk);
+  tb.rst(rst);
+  tb.ready(ready);
+  tb.finished(finished);
+  dec.clk(clk);
+  dec.rst(rst);
+  dec.ready(ready);
+  dec.finished(finished);
+
+  // Start Simulation:
+  sc_start();
+
+  // Done.
+  return 0;
+}
+
+
+//===================================
+// READ PARAMETERS FROM COMMAND LINE:
+//===================================
+void get_arguments(int argc, char * argv[])
+{
+  if (argc != 13)
+    {
+      cout << "Usage: \n\t" << argv[0] << " <alist_fname> <stim_fname> <code_rate> <SNR> <iterations> <report_interval> <lambda> <theta> <precision> <Ymax> <alpha> <log_fname>\n";
+      exit(0);
+    }
+  else
+    {
+      p.alist = loadFile(argv[1]);
+      p.stimfilename = argv[2];
+      p.Rate = atof(argv[3]);
+      p.SNR = atof(argv[4]);
+      p.iterations_per_frame = atoi(argv[5]);
+      p.total_clock_cycles = atoi(argv[6]);
+      p.lambda = atof(argv[7]);
+      p.theta = atof(argv[8]);
+      p.precision = atoi(argv[9]);
+      p.Ymax = atof(argv[10]);
+      p.alpha = atof(argv[11]);
+
+      stringstream ss;
+      ss << argv[12] << ".dat";
+      p.fname = ss.str();
+
+      p.N = p.alist.M;
+      p.M = p.alist.N;
+      p.dv = p.alist.biggest_num_m;
+      p.dc = p.alist.biggest_num_n;
+      
+      p.nEdges = 0;
+      for (int i=0; i<p.N; i++)
+	p.nEdges += p.alist.num_mlist[i];
+
+      p.N0 = (1/p.Rate)*pow(10.0, -p.SNR/10.0);	
+      p.sigma = sqrt(p.N0/2.0);
+
+      p.Nq = pow(2.0,p.precision);
+      initializeQuantization();
+    }
+}
+
+
+//===================================
+// REPORT RESULTS TO FILE:
+//===================================
+void append_result_to_data_file(unsigned int errors, unsigned int totalbits, unsigned int word_errors, unsigned int total_words, unsigned long totalIterations)
+{
+  ofstream result_file(p.fname.c_str(),ios::app);
+  result_file <<endl << (double)errors/totalbits << "\t" << (double)word_errors/total_words << "\t" 
+	      << (double)totalIterations/total_words << "\t" 
+	      << p.SNR << "\t" << errors << "\t" << totalbits << "\t" << word_errors
+	      << "\t" << total_words << "\t" << p.theta << "\t" <<  p.lambda << "\t" << p.precision << "\t" << p.Ymax << "\t" << p.alpha;
+  result_file.close();
+}
+
+
+
