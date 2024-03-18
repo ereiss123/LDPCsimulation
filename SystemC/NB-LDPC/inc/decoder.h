ddf2a4077cd667c7b1b7071387380c928a710278
Eric Reiss <eric.reiss@usu.edu>
2024-03-18 10:33:19 -0600

    adjust spacing

--- SystemC/NB-LDPC/inc/decoder.h
+++ SystemC/NB-LDPC/inc/decoder.h
@@ -9,7 +9,7 @@
 ** This header defines the top-level interface and control behavior
 ** for a decoder. The decoder module should connect directly to the
 ** testbench module, and contains the entire modular and functional
-** heirarchy of the decoder under test. 
+** heirarchy of the decoder under test.
 ==========================================================================*/
 #ifndef DECODER_H
 #define DECODER_H
@@ -41,7 +41,7 @@ struct symnode_creator
   {
     return new symnode(name, alist.num_mlist[idx], theta, lambda, sigma);
   }
-  alist_struct alist;  
+  alist_struct alist;
   double lambda;
   double theta;
   double sigma;
@@ -57,7 +57,7 @@ struct checknode_creator
   {
     return new checknode(name, alist.num_nlist[idx]);
   }
-  alist_struct alist;  
+  alist_struct alist;
 };
 
 
@@ -65,7 +65,7 @@ struct checknode_creator
   // TOP-LEVEL IO for the DECODER:
   //-------------------------------------------
   sc_vector<sc_in<double> >             y;        // Input from channel
-  sc_vector<sc_out<bool> >              d;        // Decisions
+  sc_vector<sc_out<int> >               d;        // Decisions
   sc_in<bool>                           clk;      // Clock
   sc_in<bool>                           rst;      // Reset (new frame available)
   sc_out<bool>                          ready;    // Ready to decode
@@ -79,15 +79,15 @@ struct checknode_creator
     // SC_VECTOR INTIALIZATIONS HERE:
     y("y",p.N),
     d("d",p.N),
-    stoc("stoc",p.nEdges), 
-    ctos("ctos",p.nEdges), 
-    stop("stop",p.M), 
+    stoc("stoc",p.nEdges),
+    ctos("ctos",p.nEdges),
+    stop("stop",p.M),
     rndval("rnd",p.N),
     r("r",p.N),
-    S("S"), 
+    S("S"),
     C("C"),
-				Sd("Sd",p.N),
-				updown(p.N)
+    Sd("Sd",p.N),
+    updown(p.N)
     {
       // -----------------------------------------------
       // Initialize Symbol and Check node module arrays:
@@ -95,52 +95,50 @@ struct checknode_creator
       // See ALLOCATOR definitions near the bottom of this file
       S.init(p.N,symnode_creator(p.alist,p.theta,p.lambda,p.sigma));
       C.init(p.M,checknode_creator(p.alist));
-     
+
       // -----------------------------------------
       // Establish interleaver connections:
       // -----------------------------------------
       // The connections are defined by
       // a matrix called "mlist" which is
-      // part of the alist file. The 
+      // part of the alist file. The
       // connections are specified as follows:
       // If mlist[i][j] = k, then symnode i
       // connects to checknode k on its jth edge.
 
       vector<unsigned int>	symbol_edges(p.N,0);
       vector<unsigned int>	check_edges(p.M,0);
-		
+
       int signal_index = 0;
       S[0].rnd_in(rnd_source);
-      for (int i=0; i<p.N; i++)
-	{
-	  S[i].r(r[i]);
-	  S[i].rnd_out(rndval[i]);
-	  if (i<(p.N-1))
-	    S[i+1].rnd_in(rndval[i]);
-	  S[i].d(Sd[i]);
-	  S[i].rst(rst);
-	  S[i].clk(clk);
-	  for (int j=0; j<p.alist.num_mlist[i]; j++)
-	    {
-	      int check_node_index = p.alist.mlist[i][j] - 1;
-					
-	      S[i].to_check[j](stoc[signal_index]);
-	      C[check_node_index].to_check[check_edges[check_node_index] ](stoc[signal_index]);
-					
-	      S[i].from_check[j](ctos[signal_index]);
-	      C[check_node_index].from_check[check_edges[check_node_index] ](ctos[signal_index]);
-					
-	      check_edges[check_node_index]++;
-	      symbol_edges[i]++;					
-	      signal_index++;
-	    }
-	}
-      for (int i=0; i<p.M; i++)
-	{
-	  C[i].stop(stop[i]);
-	  C[i].clk(clk);
-	  C[i].rst(rst);
-	}
+      for (int i=0; i<p.N; i++){
+        S[i].r(r[i]);
+        S[i].rnd_out(rndval[i]);
+        if (i<(p.N-1))
+          S[i+1].rnd_in(rndval[i]);
+        S[i].d(Sd[i]);
+        S[i].rst(rst);
+        S[i].clk(clk);
+        for (int j=0; j<p.alist.num_mlist[i]; j++){
+          int check_node_index = p.alist.mlist[i][j] - 1;
+
+          S[i].to_check[j](stoc[signal_index]);
+          C[check_node_index].to_check[check_edges[check_node_index] ](stoc[signal_index]);
+
+          S[i].from_check[j](ctos[signal_index]);
+          C[check_node_index].from_check[check_edges[check_node_index] ](ctos[signal_index]);
+
+          check_edges[check_node_index]++;
+          symbol_edges[i]++;
+          signal_index++;
+        }
+      }
+
+      for (int i=0; i<p.M; i++){
+        C[i].stop(stop[i]);
+        C[i].clk(clk);
+        C[i].rst(rst);
+      }
 
       // Declare top-level control behavior:
       SC_METHOD(behavior);
@@ -157,7 +155,7 @@ struct checknode_creator
   //----------------------------------
   sc_vector<sc_signal<message_type > >	 stoc;    // Symbol-to-check messages
   sc_vector<sc_signal<message_type > >	 ctos;    // Check-to-symbol messages
-  sc_vector<sc_signal<double > >         r;       // Received input at decoder (quantized) 
+  sc_vector<sc_signal<double > >         r;       // Received input at decoder (quantized)
   sc_vector<sc_signal<bool> >            stop;    // Stopping signals from check nodes
   sc_vector<sc_signal<bool> >            Sd;      // Decision signals from symbol nodes
   sc_vector<sc_signal<double> >          rndval;
@@ -171,8 +169,8 @@ struct checknode_creator
 
   //----------------------------------
   // Declare State Variables and Parameters:
-  //---------------------------------- 
-  bool first_frame; 
+  //----------------------------------
+  bool first_frame;
   unsigned int clock_count;
   unsigned int iteration_count;
   vector<int> updown;
@@ -189,68 +187,59 @@ struct checknode_creator
     // --------------------------
     // RESET behavior:
     // --------------------------
-     if (rst.read())
-       {
-	 iteration_count = 0;
-	 finished.write(false);
-	 for (int i=0; i<p.N; i++)
-	   updown[i] = 0;
+    if (rst.read()){
+      iteration_count = 0;
+      finished.write(false);
+      for (int i=0; i<p.N; i++)
+        updown[i] = 0;
 
-	 if (first_frame)
-	   if (clock_count > p.N)
-	     {
-	       first_frame = false;
-	       ready.write(true);
-	     }
-	   else
-	     clock_count++;
-	 if (!first_frame)
-	   {
-	     // Deliver quantized inputs into the symbol nodes:
-	     for (int i=0; i<p.N; i++)
-	       r[i].write(quantize(y[i].read()));
-	   }
-       }
+      if (first_frame)
+        if (clock_count > p.N){
+          first_frame = false;
+          ready.write(true);
+        }
+        else
+          clock_count++;
+      if (!first_frame){
+        // Deliver quantized inputs into the symbol nodes:
+        for (int i=0; i<p.N; i++)
+          r[i].write(quantize(y[i].read()));
+      }
+    }
 
     // --------------------------
     // Termination behavior:
     // --------------------------
-     else if ((!finished) && (!first_frame))
-       {
-	 iteration_count++;
-	 if (SMOOTHED)
-	   {
-	     for (int i=0; i<p.N; i++)
-	       {
-		 if (iteration_count > (p.iterations_per_frame-32))
-		   updown[i] += 1-2*Sd[i].read();
-	       }
-	   }
+    else if ((!finished) && (!first_frame)){
+      iteration_count++;
+      if (SMOOTHED){
+        for (int i=0; i<p.N; i++){
+          if (iteration_count > (p.iterations_per_frame-32))
+            updown[i] += 1-2*Sd[i].read();
+        }
+      }
 
 
-	 bool all_stop = true;
-	 for (int i=0; i<p.M; i++)
-	   {
-	     if (!stop[i].read())
-	       all_stop = false;
-	   }
-	 if ((iteration_count >= p.iterations_per_frame) && SMOOTHED)
-	   {
-	     for (int i=0; i<p.N; i++)
-	       {
-		 if (updown[i] > 0)
-		   d[i].write(false);
-		 else
-		   d[i].write(true);
-	       }
-	   }
-	 else
-	   for (int i=0; i<p.N; i++)
-	     d[i].write(Sd[i].read());
+      bool all_stop = true;
+      for (int i=0; i<p.M; i++){
+        if (!stop[i].read())
+          all_stop = false;
+      }
+      if ((iteration_count >= p.iterations_per_frame) && SMOOTHED){
+        for (int i=0; i<p.N; i++){
+          if (updown[i] > 0)
+            d[i].write(false);
+          else
+            d[i].write(true);
+        }
+      }
+      else
+        for (int i=0; i<p.N; i++)
+          d[i].write(Sd[i].read());
 
-	 if (all_stop || (iteration_count > p.iterations_per_frame))
-	   finished.write(true);
-       }
+      if (all_stop || (iteration_count > p.iterations_per_frame))
+        finished.write(true);
+    }
   }
 };
 
